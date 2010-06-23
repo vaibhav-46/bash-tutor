41b192d89f06dd7296f95fd10c36f3de84157033
Vaibhav Agarwal <vaisci310@gmail.com>
2010-06-23 13:19:00 +0530

    Added comments to the parser

--- create.py
+++ create.py
@@ -1,26 +1,26 @@
-def main():
+def parser(x):
     from BeautifulSoup import BeautifulSoup
     import re
-    f=open('/home/vaibhav/temp.txt','r+')
-    f1=open('/home/vaibhav/ch.t',"r+")
+    f=open('/home/vaibhav/final/'+str(x)+'txt','r+')
+    f1=open('/home/vaibhav/final/temp.t',"r+")
     doc=f.readlines()
     soup=BeautifulSoup(''.join(doc))
     soup.prettify()
     for mysame in soup.findAll("red"):
-        p=len(mysame)
+        p=len(mysame)                                                               #This command helps us know whether there are tags within the tag
         print p
         if p==3:
-            mysame.contents[0].replaceWith("\033[0;31;40m"+mysame.contents[0])
-            mysame.contents[2].replaceWith(mysame.contents[2]+fu(mysame.parent.name))
+            mysame.contents[0].replaceWith("\033[0;31;40m"+mysame.contents[0])                                              #contents[0] contains data before the inserted tag,contents[1] contains the whole next
+            mysame.contents[2].replaceWith(mysame.contents[2]+fu(mysame.parent.name))                               #tag,contents[2]  contains the part after the inserted tags
         elif p==1:
-            mysame.contents[0].replaceWith("\033[0;31;40m"+mysame.contents[0]+fu(mysame.parent.name))
+            mysame.contents[0].replaceWith("\033[0;31;40m"+mysame.contents[0]+fu(mysame.parent.name))    #parent gives the tag just before the given tag this way we remember the previous tag
     for mysame in soup.findAll("green"):
         p=len(mysame)
         print p
         print mysame.parent.name
         if p==3:
             mysame.contents[0].replaceWith("\033[0;32;40m"+mysame.contents[0])
-            mysame.contents[2].replaceWith(mysame.contents[2]+fu(mysame.parent.name))
+            mysame.contents[2].replaceWith(mysame.contents[2]+fu(mysame.parent.name))                                   #fu is a func that reads the parent name and returns the tag for the given colour
         elif p==1:
             mysame.contents[0].replaceWith("\033[0;32;40m"+mysame.contents[0]+fu(mysame.parent.name))
     for mysame in soup.findAll("brown"):
