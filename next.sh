bb2fda3ef480442ac4a5bb731ad28a7954fbbd21
Vaibhav Agarwal <vaisci310@gmail.com>
2010-07-25 19:54:16 +0530

    README file updated

--- next.sh
+++ next.sh
@@ -5,19 +5,10 @@ export x
 y=$(($x+1))
 export y
 let NUM_FILE=$((`ls [0-9]*.txt| wc -l `))
-echo >out.txt
-echo >out
-echo >temp.txt
 printf "\e[8;70;180;t"
 clear
-python create.py $x `echo $PWD`
-python new.py `echo $PWD`
-sed -e 's/<.\{,7\}>//g' <out.txt >out
+python create.py $x `echo $PWD` $COLUMNS
 if [[ "$y" -gt "$NUM_FILE" ]];then
   y=1
 fi
 echo $y>file.txt
-echo -e  `cat out` 
-echo -e "\033[0m "
-rm out*                       #Removes the unrequired files
-rm temp.txt    
