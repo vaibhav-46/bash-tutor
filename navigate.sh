9df359af9d173acbd59993d9547588fa94af05f5
Kirtika <rkirti@oespirit.(none)>
2010-07-28 15:03:07 +0530

    Cleaned up the state machine code, rmeoved unnecessary files. The state machine still needs to check for boundary conditions

--- navigate.sh
+++ navigate.sh
@@ -1,58 +1,49 @@
-#!/bin/bash
+##############################################################################################################
+# File Description: Bashtutor script that provides all the navigation commands
+# Author          : Vaibhav Agarwal
+# Date            : 26/07/2010
+# Remarks         :
+#
+# Known issues    : Does not check for boundary conditions - going before the
+#                   first chapter and beyond the last chapter
+#
+# Future Work     : Listing the index
+#
+##############################################################################################################
 
-next()
+function readfile()
 {
-
-x=`cat file.txt`
-export x
-y=$(($x+1))
-export y
-let NUM_FILE=$((`ls [0-9]*.txt| wc -l `))
-echo >out.txt
-echo >out
-echo >temp.txt
-printf "\e[8;70;180;t"
-clear
-python create.py $x `echo $PWD`
-python new.py `echo $PWD`
-sed -e 's/<.\{,7\}>//g' <out.txt >out
-if [[ "$y" -gt "$NUM_FILE" ]];then
-  y=1
-fi
-echo $y>file.txt
-echo -e  `cat out`
-echo -e "\033[0m "
-rm out*
-rm temp.txt
-
-}
-
-resume()
-{
-next
-}
-
-restart()
-{
-echo 1>file.txt
-next
-}
-
-#index()--This will output an "echo ls command " that will show all the symbolic links in the code and then the user can go to the chapter that he wants
-
-
-previous()
-{
-num=`cat file.txt`
-if (( "$num" -lt "3" ))
-then
-echo "The tutorial has just started..no previous chapter"
-exit 1
-fi
-prev=$(($num-2))
-echo $prev>file.txt
-next
+        printf "\e[8;70;180;t"
+        clear
+        size=$(stty size | awk '{print $2}')
+        python create.py $CURRENT_CHAPTER $PWD $size
 }
 
 
+function next()
+{
+        CURRENT_CHAPTER=$(($CURRENT_CHAPTER + 1))
+        export CURRENT_CHAPTER
+        readfile 
+}
 
+
+function prev()
+{
+        CURRENT_CHAPTER=$(($CURRENT_CHAPTER - 1))
+        export CURRENT_CHAPTER
+        readfile
+}
+
+
+function goto()
+{
+        CURRENT_CHAPTER=$1
+        export CURRENT_CHAPTER
+        readfile 
+}
+
+function index()
+{
+    echo "To be done"
+}
--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 ##############################################################################################################
 # File Description: Bashtutor Script That Starts the tutorial
-#                   This script displays the first chapter, a partially interactive
-#                   application meant to give terminal newbies a complete picture of most available shell
-#                   utilities.
+#                   This script displays the first chapter. The tutor is a partially interactive application 
+#                   meant to give terminal newbies a complete picture of most available shell utilities.
 # Author          : Vaibhav Agarwal
 # Date            : 26/07/2010
 # Remarks         :
@@ -13,11 +12,22 @@
 #
 ##############################################################################################################
 
+# The total number of tutorials in the chapter.
+NUM_CHAPTERS=10
+# This variable needs to be persistent, hence it is exported whenever changed
+CURRENT_CHAPTER=1
 
-#!/bin/bash
-echo "2">file.txt
+export NUM_CHAPTERS
+export CURRENT_CHAPTER
+
+# Setting the terminal screen to display the tutorial
+# This enlarges the terminal to the maximum size
 printf "\e[8;70;180;t"
 clear
-size=$(stty size | awk '{print $2}')
-python create.py 1 $PWD $size
 
+# Make the required navigation functions available
+source navigate.sh
+
+# This gets the terminal width to use in the printing routine
+width=$(stty size | awk '{print $2}')
+python create.py 1 $PWD $width
