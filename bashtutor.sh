7623b61ca8c676b309818aa17a07dadfbc3996a2
Kirtika <rkirti@oespirit.(none)>
2010-08-17 23:32:13 +0530

    Useless commit. Removing a stray file and some dos linefeeds from bashtutor.sh

--- bashtutor.sh
+++ bashtutor.sh
@@ -4,3 +4,7 @@ screen -X eval "stuff './start.shq'"
 screen -X eval "stuff 'source navigate.sh'"
 screen -X eval "stuff 'clear'"
 screen -X eval "stuff './start.sh'"
+###for j in `seq 1 1000`;
+###    do 
+###     screen -X eval "stuff 'echo foo'";
+###done 
