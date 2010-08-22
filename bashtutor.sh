c5fd04180e3ae4a6882639375208c87d3b1b425e
vaibhav <vaibhav@The-Source.(none)>
2010-08-22 14:43:25 +0530

    The chapter thing works i.e F2 and F4 work

---
+++ bashtutor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+screen 
+CURRENT_CHAPTER=`cat chapter.txt`
+screen -X eval "stuff './start.shq'"
+screen -X eval "stuff 'source navigate.sh'"
+screen -X eval "stuff 'clear'"
+screen -X eval "stuff 'goto $CURRENT_CHAPTER'"
