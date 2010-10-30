618b24aaace28b45bdfe7be51ac3349e1965032c
Vaibhav <vaibhav@Source.(none)>
2010-10-30 19:04:22 +0530

    Chapters added and also arranged in a folder

--- bashstart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-screen -c screenrc
-screen -X eval "stuff './start.shq'"
-screen -X eval "stuff 'source navigate.sh'"
-screen -X eval "stuff 'clear'"
-screen -X eval "stuff './start.sh'"
